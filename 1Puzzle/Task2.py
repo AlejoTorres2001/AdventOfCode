453816746d23186420653c609788ffb5c71fcd89
Alejo <80418452+AlejoTorres2001@users.noreply.github.com>
2021-12-04 23:35:04 -0300

    formatted

--- 1Puzzle/Task2.py
+++ 1Puzzle/Task2.py
@@ -1,31 +1,30 @@
 def add_numbers(li):
-    total=0
+    total = 0
     for numbers in li:
-        total+=numbers
+        total += numbers
     return total
-with open("1Puzzle\messurements.txt","r") as file :
-    messurements=file.readlines()
-    previous =[]
-    band=True
-    counter=0
+
+
+with open("1Puzzle\messurements.txt", "r") as file:
+    messurements = file.readlines()
+    previous = []
+    flag = True
+    counter = 0
     for i in range(len(messurements)):
-        window=[]
-        if band:
+        window = []
+        if flag:
             for j in range(3):
                 try:
                     window.append(int(messurements[i+j]))
                 except:
-                    window=[]
+                    window = []
                     print("No more messurements")
-                    band=False
-                    break 
-            previous_result=add_numbers(previous)
+                    flag = False
+                    break
+            previous_result = add_numbers(previous)
             result = add_numbers(window)
-            if (result>previous_result and previous_result!=0):
-                counter+=1
-            previous=window
+            if (result > previous_result and previous_result != 0):
+                counter += 1
+            previous = window
             print(window)
-    print("Result:",counter)
-                
-        
-        
+    print("Result:", counter)
